ab24db0f3714db267d78f4d7bc598154baac60e1
Sofie Andersson <sofie@koikidsstudio.com>
2024-02-04 13:37:02 +0000

    Adds a post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-idamariasofi-djangoblog-8lzxdzg32vz.ws-eu107.gitpod.io',
+ALLOWED_HOSTS = ['8000-idamariasofi-djangoblog-8lzxdzg32vz.ws-eu108.gitpod.io',
 '.herokuapp.com']
 
 
@@ -89,6 +89,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
