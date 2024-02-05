b5be8ea1d5d65dca6cc7bddf226f501ff7ee314c
Sofie Andersson <sofie@koikidsstudio.com>
2024-02-05 10:16:13 +0000

    Added class Meta and def __str__

--- blog/models.py
+++ blog/models.py
@@ -25,3 +25,9 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
