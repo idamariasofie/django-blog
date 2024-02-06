0f66e995edb27ec37127fcf3928692fb19088b15
Sofie Andersson <sofie@koikidsstudio.com>
2024-02-06 10:59:12 +0000

    Added coder

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Post
+from .models import Event
 
 # Create your views here.
 
@@ -30,5 +30,27 @@ def post_detail(request, slug):
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {"post": post,
+         "coder": "Sofie Andersson"},
+    )
+
+class EventsList(generic.ListView):
+
+
+    model = Event
+    template_name = "index.html"
+    paginate_by = 12
+
+
+
+
+def event_detail(request, event_id):
+    
+    queryset = Event.objects.all()
+    event = get_object_or_404(queryset, event_id=event_id)
+
+    return render(
+        request,
+        "events/event_detail.html",
+        {"event": event}
     )
