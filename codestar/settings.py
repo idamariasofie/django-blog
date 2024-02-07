9e5be0d210c7131a4f908a04e5eebc8814c98639
Sofie Andersson <sofie@koikidsstudio.com>
2024-02-07 11:52:15 +0000

    Add About app

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-idamariasofi-djangoblog-8lzxdzg32vz.ws-eu108.gitpod.io',
 '.herokuapp.com']
